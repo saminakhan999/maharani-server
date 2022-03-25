ed79dd6a44f8bc6a9caf75fe202dfa17973f9fa7
saminakhan999 <samina.khan9999@gmail.com>
2022-03-25 20:57:39 +0000

    users controller

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,18 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users");
 const { verifyToken } = require("../middleware/auth");
+const User = require("../models/user");
 
 router.get("/", verifyToken, usersController.index);
 router.get("/:id", usersController.show);
+router.get("/:id/highscores", async (req, res) => {
+  try {
+    const user = await User.findById(parseInt(req.params.id));
+    const highscores = await user.highscores;
+    res.json(highscores);
+  } catch (err) {
+    res.status(404).send({ err });
+  }
+});
 
 module.exports = router;
