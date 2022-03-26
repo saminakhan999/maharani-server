6df5a27574917743d0d3b7891848f5ac91a1c86e
saminakhan999 <samina.khan9999@gmail.com>
2022-03-26 02:29:16 +0000

    pings heroku every 5 mins

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const app = require("./server");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Express now departing from port ${port}!`));
+
+var http = require("http");
+setInterval(function() {
+    http.get("https://maharani-server.herokuapp.com/");
+}, 300000); // every 5 minutes (300000
