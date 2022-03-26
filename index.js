7d723a4f411819af0eb7fe9f2388bdb1f13f2e44
saminakhan999 <samina.khan9999@gmail.com>
2022-03-26 10:59:54 +0000

    remove heroku state

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Express now departing from port ${port}!`));
 
-var http = require("http");
-setInterval(function() {
-    http.get("https://maharani-server.herokuapp.com/");
-}, 300000); // every 5 minutes (300000
